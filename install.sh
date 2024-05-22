b750af2597dd2dab5d6feb59a5ad3fa45ce2d80c
wenhai <zhangwenhai0182@outlook.com>
2024-05-22 17:31:02 +0800

    update
    Former-commit-id: 79382236c6cb9779b24d5c161e98856deddfbadf

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 basedir=`pwd`
 
 conda env create -f environment.yaml
+chmod +x $basedir/scripts/pantax
+chmod +x $basedir/scripts/data_preprocessing.sh
 cd $basedir/tools/fastix
 cargo install fastix --root ./
