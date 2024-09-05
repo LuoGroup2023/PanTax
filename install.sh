47e8b172e763c4bf3ec32236129d964e31cfb6d7
wenhai <zhangwenhai0182@outlook.com>
2024-09-05 11:17:25 +0800

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 basedir=`pwd`
 
-conda env create -f environment.yaml
+# conda env create -f environment.yaml -y
 chmod +x $basedir/scripts/pantax
 chmod +x $basedir/scripts/data_preprocessing
 cd $basedir/tools/fastix
